cd3b43f8522b2af5d07243e98fb1ec5fc760e236
Thiago Talma <thiago@thiagomt.com>
2014-02-24 20:57:44 -0300

    Complete config of the plugin and some adjustments

--- widgets/JsTree.php
+++ widgets/JsTree.php
@@ -19,12 +19,68 @@ use yii\helpers\Json;
  */
 class JsTree extends \yii\widgets\InputWidget
 {
-	public $defaults = [];
-	public $plugins = [];
-	public $types = [];
-	public $path = [];
-	public $treeData = [];
+	/**
+	 * @var array Data configuration.
+	 * If left as false the HTML inside the jstree container element is used to populate the tree (that should be an unordered list with list items).
+	 */
+	public $data = [];
 
+	/**
+	 * @var array Stores all defaults for the core
+	 */
+	public $core = [
+		'expand_selected_onload' => true,
+		'themes' => [
+			'icons' => false
+		]
+	];
+
+	/**
+	 * @var array Stores all defaults for the checkbox plugin
+	 */
+	public $checkbox = [
+		'three_state' => true,
+		'keep_selected_style' => false];
+
+	/**
+	 * @var array Stores all defaults for the contextmenu plugin
+	 */
+	public $contextmenu = [];
+
+	/**
+	 * @var array Stores all defaults for the drag'n'drop plugin
+	 */
+	public $dnd = [];
+
+	/**
+	 * @var array Stores all defaults for the search plugin
+	 */
+	public $search = [];
+
+	/**
+	 * @var string the settings function used to sort the nodes.
+	 * It is executed in the tree's context, accepts two nodes as arguments and should return `1` or `-1`.
+	 */
+	public $sort = [];
+
+	/**
+	 * @var array Stores all defaults for the state plugin
+	 */
+	public $state = [];
+
+	/**
+	 * @var array Configure which plugins will be active on an instance. Should be an array of strings, where each element is a plugin name.
+	 */
+	public $plugins = ["checkbox"];
+
+	/**
+	 * @var array Stores all defaults for the types plugin
+	 */
+	public $types = [];
+
+	/**
+	 * @inheritdoc
+	 */
 	public function init()
 	{
 		parent::init();
@@ -45,12 +101,18 @@ class JsTree extends \yii\widgets\InputWidget
 		$view = $this->getView();
 		JsTreeAsset::register($view);
 
-		$default = array_merge($this->defaults, [
+		$config = [
+			'core' => array_merge(['data' => $this->data], $this->core),
+			'checkbox' => $this->checkbox,
+			'contextmenu' => $this->contextmenu,
+			'dnd' => $this->dnd,
+			'search' => $this->search,
+			'sort' => $this->sort,
+			'state' => $this->state,
 			'plugins' => $this->plugins,
-			'types' => $this->types,
-		]);
-
-		$json = Json::encode($default);
+			'types' => $this->types
+		];
+		$defaults = Json::encode($config);
 
 		$inputId = Html::getInputId($this->model, $this->attribute);
 
@@ -63,7 +125,7 @@ class JsTree extends \yii\widgets\InputWidget
 		.bind("changed.jstree", function(e, data, x){
 				$("#{$inputId}").val(JSON.stringify(data.selected));
 		})
-		.jstree({$json});
+		.jstree({$defaults});
 })(window.jQuery, window, document);
 SCRIPT;
 		$view->registerJs($js);
